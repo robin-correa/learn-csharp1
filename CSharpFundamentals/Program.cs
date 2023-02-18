74348781aec595ad84321bbb86968aea178683ef
Robin Correa <robin.correa21@gmail.com>
2023-02-18 12:21:01 +0800

    Learn Reference and Value types in C#.

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -82,6 +82,24 @@ class Program
         var methodName = "Express";
         var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
         Console.WriteLine(shippingMethod); // Express
+
+        // [Reference Types and Value Types]
+
+        // Value Types
+        var a = 10;
+        var b = a;
+        b++; // 10
+
+        Console.WriteLine(string.Format("a: {0}, b: {1}", a, b)); // a: 10, b: 11
+
+        // Reference Types
+
+        var array1 = new int[3] { 1, 2, 3 };
+        var array2 = array1;
+        array2[0] = 0;
+
+        Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0])); // array1[0]: 0, array2[0]: 0
+
     }
 
 }
