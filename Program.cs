8e5848279386fcc1ebff3bc1ab76d7d79d83e5a5
Robin Correa <robin.correa21@gmail.com>
2023-02-18 11:24:09 +0800

    Learn classes and objects in C#.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public class Person
+    {
+        public string? FirstName; // Nullable type
+        public string? LastName; // Nullable type
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
+    static void Main(string[] args)
+    {
+        var person1 = new Person();
+        person1.FirstName = "Robin";
+        person1.LastName = "Correa";
+        person1.Introduce();
+    }
+}
